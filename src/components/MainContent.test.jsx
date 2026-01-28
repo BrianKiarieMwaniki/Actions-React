4cf94b528fb90ac435a63b19f0b5da034fdf8a72
BrianKiarieMwaniki <briank.mwaniki@gmail.com>
2026-01-28 14:28:48 +0300

    optimized cache

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -1,21 +1,20 @@
-import {render, screen} from '@testing-library/react';
-import MainContent from './MainContent';    
-import userEvent from '@testing-library/user-event';
+import { render, screen } from "@testing-library/react";
+import MainContent from "./MainContent";
+import userEvent from "@testing-library/user-event";
 
-describe('MainContent', () =>{
-    it('should render a button', () =>{
-        render(<MainContent />);
-        const button = screen.getByRole('button');
-        expect(button).toBeInTheDocument();
-    })
+describe("MainContent", () => {
+  it("should render a button", () => {
+    render(<MainContent />);
+    const button = screen.getByRole("button");
+    expect(button).toBeInTheDocument();
+  });
 
+  it("should show help area after clicking the button", async () => {
+    render(<MainContent />);
 
-    it('should show help area after clicking the button', async () =>{
-        render(<MainContent />);
+    const button = screen.getByRole("button");
+    await userEvent.click(button);
 
-        const button = screen.getByRole('button');
-        await userEvent.click(button);
-
-        expect(screen.getByTestId('help-are')).toBeInTheDocument();
-    });
-})
+    expect(screen.getByTestId("help-are")).toBeInTheDocument();
+  });
+});
